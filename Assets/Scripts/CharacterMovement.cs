b3c3766e1b9cdb327dcd06aa033dd6a3822b9ee9
Víctor Hugo Jiménez Meseguer <victorhugojimenezmeseguer@gmail.com>
2025-11-11 09:58:08 +0100

    En el script asignamos la velocidad máxima

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    public float maxSpeed = 6.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
