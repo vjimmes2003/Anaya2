bb692a7e087ea0f940ff0b81bee44b632ac783ae
Víctor Hugo Jiménez Meseguer <victorhugojimenezmeseguer@gmail.com>
2025-11-17 10:54:28 +0100

    Movimiento Horizontal Final

---
+++ AGENTS.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: Anaya2
+- Unity version: Unity 6000.0.60f1
+- Active game object:
+  - Name: Player
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -3,15 +3,23 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     public float maxSpeed = 6.0f;
+    public float moveDirection;
+    public bool FacingRight = true;
+    private Rigidbody rigidbody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveDirection = Input.GetAxis("Horizontal");
+    }
+    void FixedUpdate()
+    {
+        rigidbody.linearVelocity = new Vector2(moveDirection * maxSpeed, rigidbody.linearVelocity.y);
     }
 }
