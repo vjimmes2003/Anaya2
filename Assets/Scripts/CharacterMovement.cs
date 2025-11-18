b9bb51138288f07121907cde28eb7c0eb1f5b5f9
Víctor Hugo Jiménez Meseguer <victorhugojimenezmeseguer@gmail.com>
2025-11-18 08:07:17 +0100

    MovimientoHorizontalCorregido

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class CharacterMovement : MonoBehaviour
 {
     public float maxSpeed = 6.0f;
     public float moveDirection;
