82fee458733a0c4d02aa5004bfb5bdd669928fcb
David Jiménez Villena <davidjimenezvillena@gmail.com>
2025-11-18 09:40:23 +0100

    Volteo del personaje version final (espero)

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -2,9 +2,24 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+
+    void FixedUpdate()
+    {
+        // L�gica para voltear
+        if (moveDirection > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
     void Flip()
     {
         facingRight = !facingRight;
         transform.Rotate(Vector3.up, 180f, Space.World);
     }
 }
+
+
